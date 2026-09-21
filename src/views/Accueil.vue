5e6613272a0c6c359b351c16a25b2164d9696f88
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2026-09-21 16:43:24 +0200

    pretty

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -17,11 +17,18 @@ defineProps({
   <section class="web-page home-page">
     <div class="home-intro">
       <h1>Scène Efreinée</h1>
-      <p>
-        Welcome to Scène Efreinée, a platform dedicated to theatre and improvisation exercises.
-        Explore exercises organized by level and category to prepare your sessions, develop your
-        creativity, and improve your group performance.
-      </p>
+      <h3>Theater Association</h3>
+      <div class="introBlock">
+        <p>
+          Welcome to Scène Efreinée, the association where everything is possible.
+          <br>Do you wish to be a start, a rogue or a stone ? Then you are in the right place.
+          <br><br>
+          On this plateform you will find everything to organize your sessions, from improvisation exercice to actual theater training. 
+          <br>Explore exercises organized by level and category to prepare your sessions, develop your creativity, and improve your group performance.
+        </p>
+
+      </div>
+      
     </div>
   </section>
 </template>
